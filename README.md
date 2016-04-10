dd9e4d71ee1b7f94b6a731a1c2ddea64c876642e
Andrew Hansen <arahan@uw.edu>
2016-04-09 21:39:54 -0700

    updated readme

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
- Antipodal Webtask
- Author: Andrew Hansen
+ ##Antipodal Webtask
 
- Find out what's happening on the opposite side ~~~the antipode~~~ of the world!
+ Find out what's happening on the opposite side (:sparkles:the antipode:sparkles:) of the world!
+ 
+ Uses Auth0's Webtask backendless server-side code tooling [webtask.io](https://webtask.io)
+ URL: https://webtask.it.auth0.com/api/run/wt-andrew-arahansen_com-0/antipodal?webtask_no_cache=1
 
  params:
  lat (required): requested latitude
--- antipodal.js
+++ antipodal.js
@@ -1,25 +1,6 @@
-/**
- * Antipodal Webtask
- * Author: Andrew Hansen
- *
- * Find out what's happening on the opposite side ~~~the antipode~~~ of the world!
- *
- * params:
- * lat (required): requested latitude
- * lng (required): requested longitude
- * web (optional): return format suitable for displaying on the web
- *
- * returns:
- * weather: a short description of the weather at the requested lat/lng antipode
- * 					with an emoji for flair
- * 					(currently supports cloud and sun emoji)
- * location: descriptive name of where the antipode lat/lng is located as well as the
- * 					 calculated antipode
- */
-
 var request = require('request');
 // open weather map key
-var owsKey = '219a61d5f0da3d74c09941b0cb122492';
+var owsKey = 'xxxxxxx';
 
 function weatherEmojis(description) {
   var emojis = {
